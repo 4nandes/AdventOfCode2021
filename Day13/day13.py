206d8156e855e7246f224534f651c937fd292147
unknown <4nandes@gmail.com>
2021-12-14 22:39:02 -0800

    adding day13 stuff

---
+++ Day13/day13.py
@@ -0,0 +1,16 @@
+def bigXandY(points):
+    bigX, bigY = 0
+    return bigX, bigY
+
+def buildData(data):
+
+    return grid
+    
+
+if __name__ == '__main__':
+    data = [x[:-1] for x in open("testing", "r")]
+    
+    for x in data:
+
+        print(x)
+    pass
